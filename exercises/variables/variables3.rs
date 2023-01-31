4a081083740015b4988275d0faca41ce87c0593d
afwolfe <3718652+afwolfe@users.noreply.github.com>
2023-01-30 23:07:38 -0500

    Fix failing intro and variables after merging

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,11 +1,7 @@
 // variables3.rs
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a hint.
 
-// I AM DONE
-
 fn main() {
-    let mut x = 3;
-    println!("Number {}", x);
-    x = 5; // don't change this line
+    let x: i32 = 8;
     println!("Number {}", x);
 }
